805e26734bc516fc95f97f6322c02a4654f697df
ibrahim <ibrahimljlidi23@gmail.com>
2023-02-17 10:00:05 +0100

    ajouter-des-controlles-user

---
+++ front/src/app/view-club copy/view-club-name.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewClubNameComponent } from './view-club-name.component';
+
+describe('ViewClubComponent', () => {
+  let component: ViewClubNameComponent;
+  let fixture: ComponentFixture<ViewClubNameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ViewClubNameComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ViewClubNameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
